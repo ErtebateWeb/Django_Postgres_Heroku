57cbd73e46c0ed955672e79dc8ef4ea5e2644877
ErtebateWeb <ertebatewweb@gmail.com>
2022-04-19 18:00:21 +0430

    Split setting.py to setting package/folder

---
+++ myproject/myproject/settings/__init__.py
@@ -0,0 +1,6 @@
+env_name = 'dev'
+if env_name == 'prod':
+    from .prod import *
+elif env_name == 'dev':
+    from .dev import *
+
---
+++ myproject/myproject/settings/dev.py
@@ -0,0 +1,10 @@
+from .base import *
+DEBUG = True
+# try:
+#     from backend.settings.local import *
+# except Exception:
+#     pass
+# INSTALLED_APPS += ['debug_toolbar', 'django_extensions']
+# MIDDLEWARE += [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ]
